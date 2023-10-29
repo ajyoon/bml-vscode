067a26e3f313de537937e4132a1a9a29c077db94
Andrew Yoon <andrew@nothing-to-say.org>
2023-10-29 12:16:37 -0400

    Add basic support for renaming reference ids

--- src/extension.ts
+++ src/extension.ts
@@ -27,6 +27,9 @@ export function activate(context: vscode.ExtensionContext) {
 		bmlOutput.append(result);
 	});
 
+	let renameProvider = new BMLRenameProvider();
+	context.subscriptions.push(vscode.languages.registerRenameProvider(
+		{ language: 'bml', scheme: 'file' }, renameProvider));
 	vscode.commands.registerTextEditorCommand('bml-vscode.createInlineChoice', createInlineChoice);
 	vscode.commands.registerTextEditorCommand('bml-vscode.referenceSelectedFork', referenceSelectedFork);
 	vscode.commands.registerTextEditorCommand('bml-vscode.nextBranch', nextBranch);
@@ -42,14 +45,16 @@ function createInlineChoice(editor: vscode.TextEditor, edit: vscode.TextEditorEd
 	editor.insertSnippet(snippet);
 }
 
+const ID_RE = /{([#$@!]*)([_a-zA-Z\xA0-\uFFFF][_a-zA-Z0-9\xA0-\uFFFF]+)[:}]/;
+const ID_RE_FULL_STRING = /^{([#$@!]*)([_a-zA-Z\xA0-\uFFFF][_a-zA-Z0-9\xA0-\uFFFF]+)[:}]$/;
+
 function referenceSelectedFork(editor: vscode.TextEditor, edit: vscode.TextEditorEdit, args: any[]) {
 	let selection = editor.selection;
 	let selectedText = editor.document.getText(selection);
 	if (selectedText[0] !== '{' || selectedText[selectedText.length - 1] !== '}') {
 		return;
 	}
-	let idRe = /^{([#$]*)(.+?):/;
-	let idMatch = selectedText.match(idRe);
+	let idMatch = selectedText.match(ID_RE);
 	if (!idMatch) {
 		return;
 	}
@@ -124,6 +129,108 @@ function nextBranch(editor: vscode.TextEditor, edit: vscode.TextEditorEdit, args
 			editor.selection = new vscode.Selection(cursorPos, cursorPos);
 		}
 	});
+}
+
+function charAtPos(document: vscode.TextDocument, position: vscode.Position): string {
+	const range = new vscode.Range(position, position.translate(0, 1));
+	return document.getText(range);
+}
+
+function findNearestOpenBraceBeforePosition(document: vscode.TextDocument, position: vscode.Position): vscode.Position | null {
+	while (position.line > 0 || position.character >= 0) {
+		let char = charAtPos(document, position);
+		if (char === '{') {
+			return position;
+		} else if (char === ' ') {
+			// Rough optimization check - whitespace means no match
+			return null;
+		}
+		position = position.translate(0, -1);
+	}
+	// No open brace found before `position`
+	return null;
+}
+
+function findNearestColonOrCloseBraceAfterPosition(document: vscode.TextDocument, position: vscode.Position): vscode.Position | null {
+	let docEnd = document.lineAt(document.lineCount - 1).range.end;
+	while (position.isBefore(docEnd)) {
+		let char = charAtPos(document, position);
+		if (char === ':' || char === '}') {
+			return position.translate(0, 1);
+		} else if (char === ' ') {
+			// Rough optimization check - whitespace means no match
+			return null;
+		}
+		position = position.translate(0, 1);
+	}
+	// No colon found after `position`
+	return null;
+}
+
+function selectRefId(document: vscode.TextDocument, position: vscode.Position): [string, vscode.Range] | null {
+	let startPos = findNearestOpenBraceBeforePosition(document, position);
+	if (startPos === null) {
+		return null;
+	}
+	let endPos = findNearestColonOrCloseBraceAfterPosition(document, position);
+	if (endPos === null) {
+		return null;
+	}
+	let text = document.getText(new vscode.Range(startPos, endPos));
+	let idMatch = text.match(ID_RE_FULL_STRING);
+	if (!idMatch) {
+		return null;
+	}
+	let id = idMatch[2];
+	endPos = endPos.translate(0, -1); // Trim colon
+	startPos = endPos.translate(0, -id.length); // Trim left-side declaration chars
+	return [
+		id, new vscode.Range(startPos, endPos)
+	];
+}
+
+/*
+Basic functionality for renaming references within the open file. This assumes
+there is no whitespace or comments between the ID and its preceding or following
+expected tokens (braces, !@#$, or colon). This also assumes IDs are not bound
+multiple times, as it simply finds the ID at the cursor and replaces all
+apparent occurrences of that ID in the document.
+*/
+class BMLRenameProvider implements vscode.RenameProvider {
+
+	async prepareRename(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Promise<vscode.Range | { placeholder: string, range: vscode.Range }> {
+		let selectRefResult = selectRefId(document, position);
+		if (selectRefResult === null) {
+			return Promise.reject();
+		}
+		let [id, replaceRange] = selectRefResult;
+		return { placeholder: id, range: replaceRange };
+	}
+
+	async provideRenameEdits(document: vscode.TextDocument, position: vscode.Position, newName: string, token: vscode.CancellationToken): Promise<vscode.WorkspaceEdit | undefined> {
+		const workspaceEdit = new vscode.WorkspaceEdit();
+		const ranges: vscode.Range[] = [];
+
+		let selectRefResult = selectRefId(document, position);
+		if (selectRefResult === null) {
+			return Promise.reject();
+		}
+		const [id, _] = selectRefResult;
+		const renameRE = new RegExp(`{([#$@!]*)(${id})[:}]`, "g");
+		for (let i = 0; i < document.lineCount; i++) {
+			let line = document.lineAt(i).text;
+			for (let match of line.matchAll(renameRE)) {
+				let endPos = new vscode.Position(i, match.index! + match[0].length - 1);
+				let startPos = endPos.translate(0, -match[2].length);
+				ranges.push(new vscode.Range(startPos, endPos));
+			}
+		}
+
+		for (const range of ranges) {
+			workspaceEdit.replace(document.uri, range, newName);
+		}
+		return workspaceEdit;
+	}
 }
 
 // this method is called when your extension is deactivated
